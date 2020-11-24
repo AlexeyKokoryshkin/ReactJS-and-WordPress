fb51dcc22e5d6a8afff414a39875435cc2a70be4
akokoryshkin <akokoryshkin@prosv.ru>
2020-11-24 10:39:08 +0300

    clear files

--- digital.prosv.ru/react-src/src/index.js
+++ digital.prosv.ru/react-src/src/index.js
@@ -1,5 +1,3 @@
-import * as serviceWorker from './serviceWorker';
-
 import React from 'react';
 import ReactDom from 'react-dom';
 import './components/styles/style.css';
@@ -14,7 +12,6 @@ import ToUser from './components/pages/ToUser';
 import ToLibrarian from './components/pages/ToLibrarian';
 import ToDistributor from './components/pages/ToDistributor';
 import Faq from './components/pages/Faq';
-//import DownloadApp from './components/pages/DownloadApp';
 
 import PageNotFound from './components/pages/PageNotFound';
 import PostPage from './components/componentsOfNews/PostPage';
@@ -33,7 +30,6 @@ ReactDom.render(<Router history={browserHistory}>
         <Route path="/test-digital/toLibrarian" component={ToLibrarian}/>
         <Route path="/test-digital/toDistributor" component={ToDistributor}/>
         <Route path="/test-digital/faq" component={Faq}/>
-        {/* <Route path="/test-digital/downloadApp" component={DownloadApp}/> */}
         <Route path="/test-digital/buying" component={Buying} />
         <Route path="/test-digital/buying-ur" component={BuyingUr} />
         <Route path="/test-digital/allnews" component={AllNews} />
@@ -42,10 +38,3 @@ ReactDom.render(<Router history={browserHistory}>
         <Route path="*" component={PageNotFound}/>
     </Route>
 </Router>, document.querySelector('#root'));
-
-
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
